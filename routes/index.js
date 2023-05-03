a15593034e6923ff654c7ba16bd82e82e4314664
Vitaly <avt0007@mail.ru>
2023-05-03 17:11:17 +0500

    fix исправить ошибку

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const routerCards = require('./cards');
 const routerUsers = require('./users');
+const { NOT_FOUND_404 } = require('../utils/constants');
 
 router.use('/users', routerUsers);
 router.use('/cards', routerCards);
+router.use('*', (req, res) => {
+  res.status(NOT_FOUND_404).send({
+    message: 'Страница не найдена',
+  });
+});
 
 module.exports = router;
