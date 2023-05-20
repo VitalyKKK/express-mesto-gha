da62aac9f2ad3f00e8767d4dc55783220c6e613d
Vitaly <avt0007@mail.ru>
2023-05-20 18:43:29 +0500

    feat routs cardId validate

--- routes/cards.js
+++ routes/cards.js
@@ -4,13 +4,13 @@ const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 const {
-  validateCardData, validateIdCardData
+  validateCardData, validateCardId,
 } = require('../middlewares/validation/cardValidate');
 
 routerCard.post('/', validateCardData, createCard);
 routerCard.get('/', getCards);
-routerCard.delete('/:id', deleteCard);
-routerCard.put('/:id/likes',validateIdCardData, likeCard);
-routerCard.delete('/:id/likes', dislikeCard);
+routerCard.delete('/:id', validateCardId, deleteCard);
+routerCard.put('/:id/likes',validateCardId, likeCard);
+routerCard.delete('/:id/likes', validateCardId, dislikeCard);
 
 module.exports = routerCard;
