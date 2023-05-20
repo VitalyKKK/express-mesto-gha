5e4e72cd9de25b59768b1e856b7108de06889359
Vitaly <avt0007@mail.ru>
2023-05-20 18:12:53 +0500

    feat cardValidate

---
+++ middlewares/validation/cardValidate.js
@@ -0,0 +1,20 @@
+const { celebrate, Joi } = require('celebrate');
+const { regExLink } = require('../../utils/constants');
+
+const validateCardData = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(regExLink),
+  }),
+});
+
+const validateIdCardData = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+});
+
+module.exports = {
+  validateCardData,
+  validateIdCardData,
+};
--- routes/cards.js
+++ routes/cards.js
@@ -3,11 +3,14 @@ const routerCard = require('express').Router();
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
+const {
+  validateCardData, validateIdCardData
+} = require('../middlewares/validation/cardValidate');
 
-routerCard.post('/', createCard);
+routerCard.post('/', validateCardData, createCard);
 routerCard.get('/', getCards);
 routerCard.delete('/:id', deleteCard);
-routerCard.put('/:id/likes', likeCard);
+routerCard.put('/:id/likes',validateIdCardData, likeCard);
 routerCard.delete('/:id/likes', dislikeCard);
 
 module.exports = routerCard;
