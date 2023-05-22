6c77fc2e14061c74d7dbe0b68a5fc06d8b4c102a
Vitaly <avt0007@mail.ru>
2023-05-22 19:59:10 +0500

    fix рефакторинг 2

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const { ObjectId } = mongoose.Schema.Types;
 const cardSchema = new mongoose.Schema({
@@ -11,6 +12,10 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
   },
   owner: {
     type: ObjectId,
